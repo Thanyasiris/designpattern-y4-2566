e2ccf7fda322bade893bc9e5d6ec5176e8b6cd08
Thanyasiri <63050146@kmitl.ac.th>
2023-12-16 20:04:08 +0700

    adjust

--- exercise1/question2/Foot.java
+++ exercise1/question2/Foot.java
@@ -22,24 +22,4 @@ public class Foot {
                 break;
         }
     }
-
-    public static void main(String[] args) {
-        FootShape footShape = new FootShape();
-        String shape = null;
-        Scanner inp = new Scanner(System.in);
-        System.out.print("What to draw? 1. Ellipse, 2. Rectangle ");
-        int type = inp.nextInt();
-        switch (type) {
-            case 1:
-                shape = "Ellipse";
-                break;
-            case 2:
-                shape = "Rectangle";
-                break;
-            default:
-                break;
-        }
-        Foot foot = new Foot(footShape);
-        foot.draw(shape);
-    }
 }
